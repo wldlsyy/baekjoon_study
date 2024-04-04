bad27e91c3dead96b9529a65ae4c20737762bef1
Jeein Stella Yang <88418078+wldlsyy@users.noreply.github.com>
2024-04-04 23:34:29 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 568 ms, Memory: 58620 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+visited = [0]*(N+1)
+
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for i in range(1, N+1):
+    graph[i].sort(reverse=True) # 정점 오름차순 정렬
+
+cnt = 1
+def bfs():
+    global cnt
+    q=deque([R])
+    visited[R] = cnt
+
+    while q:
+        x = q.popleft()
+        for point in graph[x]:
+            if visited[point] == 0:
+                cnt+=1
+                q.append(point)
+                visited[point] = cnt
+
+bfs()
+for x in range(1, N+1):
+    print(visited[x])
