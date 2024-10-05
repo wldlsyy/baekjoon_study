61f8ceeea56255b19fd57e6266d49455970abfcb
Jeein Stella Yang <88418078+wldlsyy@users.noreply.github.com>
2024-10-05 18:00:35 +0900

    [Bronze III] Title: Baseball, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10214. Baseball/Baseball.py
+++ 백준/Bronze/10214. Baseball/Baseball.py
@@ -1,22 +1,26 @@
 # 입력받기
 T = int(input())
-s = dict()
-s['Y'] = [[0 for _ in range(9)] for _ in range(T)]
-s['K'] = [[0 for _ in range(9)] for _ in range(T)]
+result = [0] * T
 
 for i in range(T):
+    ys = 0
+    ks = 0
     for j in range(9):
         y, k = map(int, input().split())
-        s['Y'][i][j] = y
-        s['K'][i][j] = k
-
-for i in range(T):
-    ys = sum(s['Y'][i])
-    ks = sum(s['K'][i])
+        ys += y
+        ks += k
 
     if ys > ks:
-        print("Yonsei")
+        result[i] = 1
     elif ys < ks:
+        result[i] = 2
+    else:
+        result[i] = 0
+
+for r in result:
+    if r == 1:
+        print("Yonsei")
+    elif r == 2:
         print("Korea")
     else:
         print("Draw")
