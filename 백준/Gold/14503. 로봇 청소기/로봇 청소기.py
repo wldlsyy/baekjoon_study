cbd2a441f2e98935e38eca699d087e8d256fe976
Jeein Stella Yang <88418078+wldlsyy@users.noreply.github.com>
2024-10-08 23:19:52 +0900

    [Gold V] Title: 로봇 청소기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,88 +1,96 @@
-from collections import deque
-# 입력 받기
-n, m = map(int, input().split())
+N, M = map(int, input().split())
 r, c, d = map(int, input().split())
-room = []
-for _ in range(n):
-    room.append(list(map(int, input().split())))
+room = [list(map(int, input().split())) for _ in range(N)]
+visited = [[0 for _ in range(M)] for _ in range(N)]
 
-# 북, 동, 남, 서 -> d로 들어오는 값이 인덱스가 됨!
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-def rear(x, y, d):
-    # 바라보는 방향 기준, 뒤쪽으로 이동된 좌표 반환
-    if d == 0: # 북 -> 남
-        nx = x + dx[2]
-        ny = y + dy[2]
-    elif d == 1: # 동 -> 서
-        nx = x + dx[3]
-        ny = y + dy[3]
-    elif d == 2: # 남 -> 북
-        nx = x + dx[0]
-        ny = y + dy[0]
-    else: # 서 -> 동
-        nx = x + dx[1]
-        ny = y + dy[1]
-    return nx, ny
+def print_visited():
+    for i in range(N):
+        for j in range(M):
+            print(visited[i][j], end=" ")
+        print()
+class Robot():
+    def __init__(self, r, c, d):
+        self.r = r
+        self.c = c
+        self.d = d
+        self.cnt = 0
 
-def change_dir(d):
-    # 반시계 방향 90도 회전
-    dir = {0: 3, 3: 2, 2: 1, 1: 0}
+    # 현재 칸 청소 가능하면 청소
+    def cleanRoom(self):
+        if not visited[self.r][self.c] and room[self.r][self.c] == 0:
+            # print(f'{(self.r, self.c)} 청소 가능')
+            visited[self.r][self.c] = 1
+            self.cnt += 1
+            # print(f'--청소 완료-- 청소한 칸 개수: {self.cnt}')
+            # print_visited()
 
-    return dir[d]
+    # 현재 칸 주변 4칸 중 청소 가능한 구역이 있는지 확인
+    def isCleanable(self):
+        for i in range(4):
+            self.changeDir()
+            nr = self.r + dx[self.d]
+            nc = self.c + dy[self.d]
 
-def find_next_cleanable(x, y, d, visited):
-    #print(f'현재 방향: {d}')
-    for i in range(4):
-        d = change_dir(d) # 반시계 방향으로 회전
-        nx = x + dx[d]
-        ny = y + dy[d]
+            # 청소 가능한 칸 찾기
+            if not visited[nr][nc] and room[nr][nc] == 0:
+                # print(f'({nr, nc})로 전진 가능. 방향: {self.d}')
+                return True
+        return False
 
-        # 청소 가능한 칸
-        if not visited[nx][ny] and room[nx][ny] != 1:
-            #print(f'({nx, ny})로 전진 가능. 방향: {d}')
-            return nx, ny, d
+    def canMoveBackwards(self):
+        nr = self.r - dx[self.d]
+        nc = self.c - dy[self.d]
 
-    return None
-
-def bfs(r, c, d):
-    q = deque([(r, c, d)])
-    visited = [[False for _ in range(m)] for _ in range(n)]
-
-    cnt = 0 # 청소하는 칸 카운트
-    while q:
-        x, y, d = q.popleft()
-        #print(f'현재 위치와 방향: ({x, y}), {d}')
-
-        # 동작 1: 현재 칸 청소 가능하면 청소
-        if room[x][y] == 0 and not visited[x][y]: # 현재 위치가 청소되지 않은 칸이면
-            #print(f'({x, y}) 청소 가능')
-            visited[x][y] = True # 현재 칸 청소
-            cnt += 1
-            #print(f'--청소 완료-- 청소한 칸 개수: {cnt}')
-
-        # 현재 칸 주변 4칸이 어떤 상태인지 파악
-        # 동작 2: 현재 칸 주변 4칸 중 청소되지 않은 빈칸이 있는지 확인
-        next_cleanable = find_next_cleanable(x, y, d, visited)
-
-        if next_cleanable:
-            # 청소 가능한 칸이 있으면 전진
-            nx, ny, nd = next_cleanable
-            q.append((nx, ny, nd))
+        if room[nr][nc] != 1:
+            return True
         else:
-            # 4방향 모두 청소할 수 없을 때 후진
-            rx, ry = rear(x, y, d)
-            #print(f'({rx, ry})로 후진 가능한지 확인')
+            # print('Cannot Move')
+            return False
 
-            if room[rx][ry] == 1:
-                #print(f'({rx, ry})로 후진 불가능 -> 작동 종료')
-                return cnt
-            else:
-                #print(f'({rx, ry})로 후진')
-                q.append((rx, ry, d))
+    # 방향 유지한 채 한칸 후진
+    def moveBack(self):
+        self.r = self.r - dx[self.d]
+        self.c = self.c - dy[self.d]
 
-    return cnt
+    # 방향 유지한 채 한칸 전진
+    def moveForward(self):
+        self.r = self.r + dx[self.d]
+        self.c = self.c + dy[self.d]
 
-result = bfs(r, c, d)
-print(result)
+    # 반시계 회전
+    def changeDir(self):
+        self.d = (self.d + 3) % 4
+        # print(f'회전된 방향: {self.d}')
+
+    # 로봇의 현재 좌표와 방향 출력
+    def printStatus(self):
+        print(f'현재 위치: {(self.r, self.c)}, 현재 방향: {self.d}')
+
+robot = Robot(r, c, d)
+
+while True:
+    # 동작 1: 현재 칸 청소 가능하면 청소
+    robot.cleanRoom()
+
+    # 현재 칸 주변 4칸이 어떤 상태인지 파악
+    if robot.isCleanable():
+        # 동작 2: 현재 칸 주변 4칸 중 청소되지 않은 빈칸이 있는 경우
+        # 반시계 회전해 청소 가능한 위치 찾기
+        # 청소 가능한 칸이 있으면 그 방향으로 전진 -> 다시 동작 1로
+        robot.moveForward()
+        continue
+
+    # 동작 3: 현재 칸 주변 4칸 중 청소되지 않은 빈칸이 없는 경우
+    else:
+        # 4 방향 모두 청소할 수 없을 때 후진 시도
+        if robot.canMoveBackwards():
+            # 후진 가능하면 후진하고 다시 동작 1로
+            robot.moveBack()
+            continue
+        else:
+            # 후진 못하면 작동 종료
+            print(robot.cnt)
+            break
